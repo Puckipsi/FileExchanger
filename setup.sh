3f91eb322101f1f541d8e630ccef2679988b3c0f
puckipsi <angelskijm@gmail.com>
2023-04-14 15:59:31 +0300

    update requirements and setup script

--- setup.sh
+++ setup.sh
@@ -1,15 +1,18 @@
 
 #run on debian based
 
-sudo apt-get update
+#run manual:
+# 1. git clone https://github.com/Puckipsi/FileExchanger.git
+# 2. cd FileExchanger
+# 3. bash setup.sh --verbose
+
+yes | sudo apt-get update
+
+yes | sudo apt install redis-server
 
 yes | sudo apt-get install python3-venv
 
 yes | sudo apt install python3-pip
-
-git clone https://github.com/Puckipsi/FileExchanger.git
-
-cd FileExchanger
 
 source venv/bin/activate
 
