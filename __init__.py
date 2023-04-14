9f673c9bb853bbb76e6f74005f4ae26aefcd74c7
puckipsi <angelskijm@gmail.com>
2023-04-14 14:19:05 +0300

    add celery

--- __init__.py
+++ __init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from celery import Celery
+from utils.celery import get_celery_app_instance
+
 
 
 app = Flask(__name__, 
             template_folder='application/templates',
             static_folder='application/static')
 
+celery = get_celery_app_instance(app)
 
 import application.routes
